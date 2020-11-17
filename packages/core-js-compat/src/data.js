3a2d702f8d9254da5588199928728ff406ebe076
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 06:03:54 +0700

    remove `esnext.string.replace-all` (moved to stable es)

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1479,7 +1479,6 @@ const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.string.match-all': null,
-  'esnext.string.replace-all': null,
   'esnext.symbol.async-dispose': {
   },
   'esnext.symbol.dispose': {
@@ -1487,9 +1486,6 @@ const data = {
   'esnext.symbol.observable': {
   },
   'esnext.symbol.pattern-match': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.symbol.replace-all': {
   },
   'esnext.typed-array.at': {
     chrome: '89',
@@ -1568,6 +1564,5 @@ const data = {
 data['esnext.aggregate-error'] = data['es.aggregate-error'];
 data['esnext.promise.any'] = data['es.promise.any'];
 data['esnext.string.match-all'] = data['es.string.match-all'];
-data['esnext.string.replace-all'] = data['es.string.replace-all'];
 
 module.exports = data;
